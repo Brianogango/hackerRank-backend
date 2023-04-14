b34341aed1dbeed74d334d0c0ff440484fa62053
minnehkamau <minnieches08@gmail.com>
2023-04-14 05:06:30 +0300

    generate kata resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_082945) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_113210) do
+  create_table "kata", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+    t.string "url"
+    t.string "category"
+    t.string "description"
+    t.string "tags"
+    t.string "languages"
+    t.json "rank"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
