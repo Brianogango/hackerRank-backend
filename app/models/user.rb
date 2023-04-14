0a287be0e4e110711992c1c239746f6590098987
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-14 09:33:02 +0300

    create usercontroller routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username,  presence: true
-    validates :userType, presence: true
+    validates :userType, inclusion: { in: %w[student TM], message: "must be either 'student' or 'TM'" }
     validates :password,  presence: true
 
     validates :email,presence:true, format: { with: URI::MailTo::EMAIL_REGEXP }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
    post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post '/users', to: 'users#create'
 end
