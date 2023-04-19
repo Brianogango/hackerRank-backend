5337dfbe2a8cf18ee4b711b6812261b3b0ca291f
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-19 13:32:26 +0300

    remove bugs

--- db/migrate/20230413113210_create_kata.rb
+++ db/migrate/20230413113210_create_kata.rb
@@ -1,6 +1,6 @@
 class CreateKata < ActiveRecord::Migration[7.0]
   def change
-    create_table :katas do |t|
+    create_table :kata do |t|
       t.string :name
       t.string :slug
       t.string :url
