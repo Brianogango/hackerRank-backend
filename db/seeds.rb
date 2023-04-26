e4ae7b5480ef5b904a8a82ba0d403a9186e90db2
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-26 12:07:51 +0300

    users controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-puts "Seeding DB⏳..."
+# puts "Seeding DB⏳..."
 
-puts "Seeding Katas⏳..."
+# puts "Seeding Katas⏳..."
 Kata.create({
   name: "Sort array by string length",
   slug: "sort-array-by-string-length",
@@ -69,22 +69,168 @@ Kata.create({
     ],
   },
 );
+Kata.create({
+name: "Add Two Numbers in String Format",
+slug: "add-two-numbers-in-string-format",
+category: "algorithm",
+languages: ["javascript"],
+url: "https://www.codewars.com/kata/5277c8a221e209d3f6000b56",
+description: "Given two non-negative integers represented as strings, your task is to add these two numbers and return it as a string. You may assume the input string will only contain digits 0-9. You cannot use any built-in BigInteger library or convert the inputs to integers directly.\n\nFor example, given the following inputs:\n\n'123' and '456'\n\nYour function should return '579'.",
 
+ rank: {
+      id: -6,
+      name: "6 kyu",
+      color: "white",
+    },
+    
+starter_code: "function addStrings(num1, num2) {\n // Write your code here\n}",
+tests: [
+{
+input: ['123', '456'],
+output: '579'
+},
+{
+input: ['0', '0'],
+output: '0'
+},
+{
+input: ['9999', '1'],
+output: '10000'
+},
+{
+input: ['1', '999'],
+output: '1000'
+},
+{
+input: ['999999', '1'],
+output: '1000000'
+}
+]
+});
+Kata.create({
+  name: "Add Two Numbers",
+  slug: "add-two-numbers",
+  category: "algorithm",
+  languages: ["javascript"],
+  url: "https://www.codewars.com/kata/5294472124b67eaac00000a5",
+  description: "Write a function that adds two numbers. You should not use + or any arithmetic operators.",
+  rank: {
+    id: -8,
+    name: "8 kyu",
+    color: "white",
+  },
+  starter_code: "function add(num1, num2) {\n  // Write your code here\n}",
+  tests: [
+    {
+      input: [2, 3],
+      output: 5,
+    },
+    {
+      input: [-1, 5],
+      output: 4,
+    },
+    {
+      input: [-4, -8],
+      output: -12,
+    },
+    {
+      input: [0, 0],
+      output: 0,
+    },
+    {
+      input: [100000, 100000],
+      output: 200000,
+    },
+  ],
+});
 
+Kata.create({
+name: "Reverse a String",
+slug: "reverse-a-string",
+category: "algorithm",
+languages: ["javascript"],
+url: "https://www.codewars.com/kata/5168bb5dfe9a00b126000018",
+description: "Write a function that takes a string as input and returns the string reversed. You should not use the built-in reverse() method.",
+rank: {
+id: 8,
+name: "8 kyu",
+color: "white",
+},
+starter_code: "function reverseString(str) {\n // Write your code here\n}",
+tests: [
+{
+input: ["hello"],
+output: "olleh",
+},
+{
+input: ["123456789"],
+output: "987654321",
+},
+{
+input: ["racecar"],
+output: "racecar",
+},
+{
+input: ["coding"],
+output: "gnidoc",
+},
+{
+input: ["hello world"],
+output: "dlrow olleh",
+},
+],
+});
+
+Kata.create({
+name: "Find the Smallest Number",
+slug: "find-the-smallest-number",
+category: "algorithm",
+languages: ["javascript"],
+url: "https://www.codewars.com/kata/55a2d7ebe362935a210000b2",
+description: "Write a function that takes an array of numbers as input and returns the smallest number in the array. You should not use the built-in Math.min() method.",
+rank: {
+id: 8,
+name: "8 kyu",
+color: "white",
+},
+starter_code: "function findSmallestNum(arr) {\n // Write your code here\n}",
+tests: [
+{
+input: [[1, 2, 3]],
+output: 1,
+},
+{
+input: [[5, 4, 3, 2, 1]],
+output: 1,
+},
+{
+input: [[10, 0, 100, -10]],
+output: -10,
+},
+{
+input: [[0, 0, 0, 0]],
+output: 0,
+},
+{
+input: [[-5, -4, -3, -2, -1]],
+output: -5,
+},
+],
+});
 
   # Assessment.create(title: "Ruby Basics", duration: "60 minutes")
   # Assessment.create(title: "Javascript Basics", duration: "60 minutes")
 
 
-  puts "Seeding Users⏳..."
+#   puts "Seeding Users⏳..."
 
-User.create(username: "minnie", email: "minnie@gmail.com", userType: "TM", password: "123456") 
-User.create(username: "blue", email: "blue@gmail.com", userType: "student", password: "123456")
-User.create(username: "faith" , email: "faith@gmail.com", userType: "student", password: "123456")
-User.create(username: "green", email: "green@gmail.com", userType: "student", password: "123456")
-User.create(username: "red", email: "red@gmail.com", userType: "student", password: "123456")
-User.create(username: "white" , email: "white@gmail.com", userType: "student", password: "123456")
-User.create(username: "joy", email: "joy@gmail.com", userType: "student", password: "123456")
+# User.create(username: "minnie", email: "minnie@gmail.com", userType: "TM", password: "123456") 
+# User.create(username: "blue", email: "blue@gmail.com", userType: "student", password: "123456")
+# User.create(username: "faith" , email: "faith@gmail.com", userType: "student", password: "123456")
+# User.create(username: "green", email: "green@gmail.com", userType: "student", password: "123456")
+# User.create(username: "red", email: "red@gmail.com", userType: "student", password: "123456")
+# User.create(username: "white" , email: "white@gmail.com", userType: "student", password: "123456")
+# User.create(username: "joy", email: "joy@gmail.com", userType: "student", password: "123456")
 
 # puts "Seeding Invitations⏳..."
 # Invitation.create(assessment_id: 1, user_id: 2, status: 'pending', note: 'The javascript assesment is created to test your skills', email: 'blue@gmail.com', end_date: '2023-04-18')
