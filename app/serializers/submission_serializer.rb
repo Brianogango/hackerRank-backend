41039522a6a03de07b111b9b6471b939aae911f5
brianonyango <brian.ogango@student.moringaschool.com>
2023-05-01 11:51:31 +0300

    add percentage column to submissions table

--- app/serializers/submission_serializer.rb
+++ app/serializers/submission_serializer.rb
@@ -1,5 +1,5 @@
 class SubmissionSerializer < ActiveModel::Serializer
-  attributes :id, :assessment_id, :kata_id, :code, :result
+  attributes :id, :assessment_id, :kata_id, :code, :result, :percentage
     belongs_to :user
   belongs_to :assessment
   belongs_to :kata
